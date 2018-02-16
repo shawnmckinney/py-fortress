8ebde0f894b74a25c30a5f0859ce5bf7f3aa3c33
Shawn McKinney <smckinney@apache.org>
2018-02-16 14:58:44 -0600

    add the session entity

---
+++ model/session.py
@@ -0,0 +1,32 @@
+'''
+Created on Feb 16, 2018
+
+@author: smckinn
+'''
+# Copyright 2018 - Symas Corporation
+
+class Session:    
+    "Fortess Session"
+    def __init__(
+            self,
+            user=None,
+            is_authenticated=False,
+            session_id=None,
+            last_access=None,
+            timeout=None,
+            error_id=None,
+            expiration_seconds=None,
+            grace_logins=None,
+            message=None,
+            warnings=None
+            ):
+        self.user=user
+        self.is_authenticated=is_authenticated
+        self.session_id=session_id
+        self.last_access=last_access
+        self.timeout=timeout
+        self.error_id=error_id
+        self.expiration_seconds=expiration_seconds
+        self.grace_logins=grace_logins
+        self.message=message
+        self.warnings=warnings
