3757f401fb22bb2f9b70bb354b46a130023c6092
Emily Backes <lucca@accela.net>
2018-03-30 08:56:12 -0700

    implement wildcard match in file userdao

--- test/test_file_daos.py
+++ test/test_file_daos.py
@@ -3,6 +3,7 @@ from file import userdao
 from model import User
 from test.utils import print_user, print_role, print_ln, print_entity
 import user_test_data
+from json import dumps
 
 class BasicTestSuite(unittest.TestCase):
     """These tests assume fortress user and permission data has been pre-loaded into File, i.e. via apache fortress administrative functions."""
@@ -18,17 +19,30 @@ class TestDaos(unittest.TestCase):
         """
         print_ln('test search users by uid')
         try:
-            usr = User(uid = "testuser1")
+            usr = User(uid = "foo*")
             uList = userdao.search(usr)
             for idx, entity in enumerate(uList) :
                 print_user(entity, "User[" + str(idx+1) + "]:")
         except Exception as e:
             self.fail('user search failed, exception=' + e.msg)
 
+    def test_search_wild(self):
+        """
+        Test wildcard match
+        """
+        print_ln('test wildmatch users')
+        us = userdao.search(User(uid="test*"))
+        try:
+            us = sorted(map (lambda u: u.uid, us))
+        except Exception as e:
+            self.fail('exception '+e.msg)
+        if us != ['testuser1', 'testuser2']:
+            self.fail('wrong set of users in match: '+dumps(us))
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(TestDaos('test_search_users'))
+    suite.addTest(TestDaos('test_search_wild'))
     return suite
 
 
